16872d94e1730f5db9fc19541a06edb4754f0d78
Priyanshidevi7059 <priyanshidevi7059@gmail.com>
2026-09-13 17:04:58 +0530

    Day 02: Variables and Data Types

---
+++ Day2.py
@@ -0,0 +1,16 @@
+# Day 02 - Variables & Data Types
+
+name = "Priyanshi"
+age = 21
+height = 5.4
+is_student = True
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
---
+++ Practice.py
@@ -0,0 +1,30 @@
+"""num = int(input("Enter the number:"))
+if num >= 0:
+elif num<0:
+    print ("Negative")
+else:
+    print("Zero") 
+
+marks = int(input("Enter the marks:"))
+if marks >= 90:
+    print("A")
+elif marks >= 75:
+    print("B")  
+elif marks >= 60:
+    print("C")   
+elif marks >= 40:
+    print("D") 
+elif marks >= 33:
+    print("Pass") 
+else:
+    print("Fail")
+    """
+age = int(input("Enter the age:"))
+if age <= 13:
+    print("Child")
+elif age <= 19:
+    print("Teenager")    
+elif age <= 59:
+    print ("Adult")    
+else:
+    print("Senior Citizen")    
